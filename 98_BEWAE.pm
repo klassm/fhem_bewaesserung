ba4019c5aec3ad4f24ef59068e098cda29167eec
Matthias Klass <matthias.klass@gmail.com>
2018-04-02 11:42:01 +0200

    Room attribute is lowercase

--- 98_BEWAE.pm
+++ 98_BEWAE.pm
@@ -78,7 +78,7 @@ BEWAE_detailFn($$$$) {
     my ($FW_wname, $d, $room, $pageHash) = @_;
     my $hash = $defs{$d};
     my $name = $hash->{NAME};
-    my $room = $hash->{ATTRIBUTES}->{ROOM};
+    my $room = $hash->{ATTRIBUTES}->{room};
     return "<div><div id=\"BEWAE\"/></div>"
         ."<script>\$.getScript('fhem/BEWAE/bewae.js', function() { bewaesserung_load_complete(\"$name\", \"$room\");});"
         ."\$('<link>').appendTo('head').attr({type : 'text/css', rel : 'stylesheet'}).attr('href', 'fhem/BEWAE/bewae.css');"
